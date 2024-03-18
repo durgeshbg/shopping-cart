c685f87d0861c8e412b392571d7f1e5a21785507
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-03-18 21:57:18 +0530

    add: Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
       <h1>Hello</h1>
+      <Navbar />
       <Outlet />
     </>
   );
