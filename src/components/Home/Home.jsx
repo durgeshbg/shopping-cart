b215d75acc308b670a8f6e71d3a390efd3ef01ae
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-03-18 23:01:12 +0530

    style Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
+import { StyledHome } from './Home.styles';
 
 const Home = () => {
   return (
     <>
+      <StyledHome>
         <p>
           Discover endless possibilities as you explore our diverse range of products,
           from the latest tech gadgets to stylish fashion trends, home essentials, and
@@ -10,6 +12,7 @@ const Home = () => {
           wardrobe, or sprucing up your living space, we&#39;ve got you covered.
         </p>
         <Link to='/shop'>Go to shopping...</Link>
+      </StyledHome>
     </>
   );
 };
---
+++ src/components/Home/Home.styles.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const StyledHome = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 80dvh;
+
+  p {
+    width: 50%;
+    line-height: 30px;
+    word-spacing: 5px;
+  }
+
+  a {
+    background-color: #525252;
+    color: white;
+    text-decoration: none;
+    padding: 5px 10px;
+    border-radius: 5px;
+  }
+
+  a:hover {
+    background-color: #d8d6d6;
+    color: #525252;
+  }
+`;
