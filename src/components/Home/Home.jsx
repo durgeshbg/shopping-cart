f56fe41d217675cda5bc75f50b523d53edfa7a57
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-03-18 23:00:35 +0530

    add Home content

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+        <p>
+          Discover endless possibilities as you explore our diverse range of products,
+          from the latest tech gadgets to stylish fashion trends, home essentials, and
+          much more. Whether you&#39;re searching for that perfect gift, upgrading your
+          wardrobe, or sprucing up your living space, we&#39;ve got you covered.
+        </p>
+        <Link to='/shop'>Go to shopping...</Link>
+    </>
+  );
 };
 
 export default Home;
