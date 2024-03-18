c6b9b6d0e50477ee1fc6f823312d66d2a54c0b4b
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-03-18 23:08:11 +0530

    refactor Navbar test

--- src/components/Navbar/Navbar.test.jsx
+++ src/components/Navbar/Navbar.test.jsx
@@ -1,16 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { expect, it } from 'vitest';
 import Navbar from './Navbar';
 import { BrowserRouter } from 'react-router-dom';
 
-describe('Navbar', () => {
-  it('Mount', () => {
-    render(
-      <BrowserRouter>
-        <Navbar />
-      </BrowserRouter>
-    );
-    const nav = screen.getByRole('navigation');
-    expect(nav).toBeInTheDocument();
-  });
+it('Navbar Mount', () => {
+  render(
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
+  const nav = screen.getByRole('navigation');
+  expect(nav).toBeInTheDocument();
 });
