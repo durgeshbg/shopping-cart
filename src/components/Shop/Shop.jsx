c00a2f1e356dfe144579728ece08066dec8e2ddb
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-03-19 19:53:56 +0530

    add: Shop styles

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,19 +1,20 @@
 import { useOutletContext } from 'react-router-dom';
+import { StyledShop } from './Shop.styles';
 import Card from '../Card/Card';
 
 const Shop = () => {
   const { data, error, loading } = useOutletContext();
 
-  if (error) return <main>{error}</main>;
-  if (loading) return <main>Loading...</main>;
+  if (error) return <StyledShop>{error}</StyledShop>;
+  if (loading) return <StyledShop>Loading...</StyledShop>;
 
   return (
     <>
-      <main>
+      <StyledShop>
         {Object.keys(data).map((id) => (
           <Card key={id} {...data[id]} />
         ))}
-      </main>
+      </StyledShop>
     </>
   );
 };
---
+++ src/components/Shop/Shop.styles.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const StyledShop = styled.main`
+  display: flex;
+  flex-wrap: wrap;
+`;
