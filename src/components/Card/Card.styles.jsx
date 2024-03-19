713362544e2e6ff6069a5037bfd07f53567da70c
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-03-20 01:15:24 +0530

    add: Cart dashboard styles

--- src/components/Card/Card.styles.jsx
+++ src/components/Card/Card.styles.jsx
@@ -53,4 +53,49 @@ export const StyledCard = styled.div`
   span {
     color: gold;
   }
+
+  .error {
+    color: red;
+    font-size: 0.7rem;
+  }
+
+  .cart {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    padding: 10px;
+  }
+
+  .quantity {
+    display: flex;
+    gap: 5px;
+  }
+
+  button {
+    font-family: inherit;
+  }
+
+  .quantity button {
+    background-color: #6666ff;
+    border: none;
+    border-radius: 5px;
+  }
+
+  input {
+    width: 40px;
+    border-radius: 5px;
+    border: 0.1px solid black;
+    padding: 0px 5px;
+    font-family: inherit;
+  }
+  input:focus {
+    outline: none;
+  }
+
+  .cart > button {
+    background-color: #6666ff;
+    border: none;
+    border-radius: 5px;
+    padding: 5px;
+  }
 `;
