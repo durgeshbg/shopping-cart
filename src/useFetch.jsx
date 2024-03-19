749e01445cb8a1e8b62fa4fa3699b9e5584bf11e
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-03-19 19:45:14 +0530

    add: custom hook to fetch data

---
+++ src/useFetch.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+export function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (res.status >= 400) {
+          throw new Error('Server error!');
+        }
+        return res.json();
+      })
+      .then((json) => setData(json))
+      .catch((err) => setError(err))
+      .finally(() => setLoading(false));
+  }, [url]);
+
+  return { data, error, loading };
+}
+
+export default useFetch;
