a5247931d2092bc96086e62a1fa2d816f89a1186
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-03-20 01:01:34 +0530

    update: Error component

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,5 +1,14 @@
+import { StyledError } from './Error.styles';
+
 const Error = () => {
-  return <h1>Error</h1>;
+  return (
+    <StyledError>
+      <h1>Error 404!</h1>
+      <div>
+        What your&apos;e looking doesn&apos;t exist. <div className='emoji'>&#9785;</div>
+      </div>
+    </StyledError>
+  );
 };
 
 export default Error;
