f745a3bec5b63072b5b7267e8aeda38b595d3a0e
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-03-20 01:15:04 +0530

    add: Cart dashboard

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,22 @@
 import PropTypes from 'prop-types';
 import { StyledCard } from './Card.styles';
+import { useState } from 'react';
 
 const Card = ({ title, category, image, price, rating }) => {
+  const [q, setQ] = useState(0);
+  const [error, setError] = useState('');
+
+  const incQ = () => setQ(Number(q) + 1);
+  const decQ = () => setQ(Number(q) - 1);
+
+  const addToCart = () => {
+    if (q <= 0) setError('Quantity invalid!');
+    else {
+      setError('');
+      setQ(0);
+    }
+  };
+
   return (
     <>
       <StyledCard>
@@ -18,6 +33,15 @@ const Card = ({ title, category, image, price, rating }) => {
               {rating.rate} - {rating.count}
             </div>
           </div>
+        </div>
+        <div className='error'>{error}</div>
+        <div className='cart'>
+          <div className='quantity'>
+            <button onClick={decQ}>-</button>
+            <input value={q} onChange={(e) => setQ(e.target.value)} />
+            <button onClick={incQ}>+</button>
+          </div>
+          <button onClick={addToCart}>Add Cart</button>
         </div>
       </StyledCard>
     </>
