5564fcf5ffc1bd13dde5378c9985e84bc3a96eeb
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-03-20 00:29:33 +0530

    add: Card test

---
+++ src/components/Card/Card.test.jsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react';
+import { expect, it } from 'vitest';
+import Card from './Card';
+
+it('Card Mount', () => {
+  const mockData = {
+    title: 'title',
+    category: 'category',
+    image: 'image',
+    price: 0,
+    rating: { rate: 5, count: 200 },
+  };
+
+  const { container } = render(<Card {...mockData} />);
+
+  const title = container.querySelector('.title');
+  expect(title.textContent).toMatch('title');
+
+  const category = container.querySelector('.category');
+  expect(category.textContent).toMatch('category');
+
+  const image = container.querySelector('img');
+  expect(image.src).toMatch('image');
+
+  const price = container.querySelector('.price');
+  expect(price.textContent).toMatch('0$');
+});
