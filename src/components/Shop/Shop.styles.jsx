160311e2f0b90c51a9b0f7d6d2ece60988e12eeb
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-03-20 00:07:19 +0530

    update Shop styles

--- src/components/Shop/Shop.styles.jsx
+++ src/components/Shop/Shop.styles.jsx
@@ -3,4 +3,7 @@ import styled from 'styled-components';
 export const StyledShop = styled.main`
   display: flex;
   flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+  padding: 1rem 0px;
 `;
