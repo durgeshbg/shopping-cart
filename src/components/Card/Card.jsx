716de5c0b9f0dbefeae1e541d5d06477133dfae4
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-03-20 01:24:52 +0530

    remove: add to cart logic

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,22 +1,8 @@
 import PropTypes from 'prop-types';
 import { StyledCard } from './Card.styles';
-import { useState } from 'react';
+import CartDashBoard from '../CartDashBoard/CartDashBoard';
 
 const Card = ({ title, category, image, price, rating }) => {
-  const [q, setQ] = useState(0);
-  const [error, setError] = useState('');
-
-  const incQ = () => setQ(Number(q) + 1);
-  const decQ = () => setQ(Number(q) - 1);
-
-  const addToCart = () => {
-    if (q <= 0) setError('Quantity invalid!');
-    else {
-      setError('');
-      setQ(0);
-    }
-  };
-
   return (
     <>
       <StyledCard>
@@ -34,15 +20,7 @@ const Card = ({ title, category, image, price, rating }) => {
             </div>
           </div>
         </div>
-        <div className='error'>{error}</div>
-        <div className='cart'>
-          <div className='quantity'>
-            <button onClick={decQ}>-</button>
-            <input value={q} onChange={(e) => setQ(e.target.value)} />
-            <button onClick={incQ}>+</button>
-          </div>
-          <button onClick={addToCart}>Add Cart</button>
-        </div>
+        <CartDashBoard />
       </StyledCard>
     </>
   );
