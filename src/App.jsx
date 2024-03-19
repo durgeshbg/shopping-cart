8ea471a91596808b28743a056e58e5792d69bedb
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-03-20 02:23:40 +0530

    add cart state and pass props to shop route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import useFetch from './useFetch';
+import { useState } from 'react';
 
 function App() {
-  const data = useFetch('https://fakestoreapi.com/products');
+  const rawdata = useFetch('https://fakestoreapi.com/products');
+  const [cart, setCart] = useState([]);
   const { pathname } = useLocation();
 
   return (
     <>
       <Navbar />
-      {pathname == '/shop' ? <Outlet context={data} /> : <Outlet />}
+      {pathname == '/shop' ? (
+        <Outlet context={{ rawdata, cart, setCart }} />
+      ) : pathname === '/cart' ? (
+        <Outlet context={cart} />
+      ) : (
+        <Outlet />
+      )}
     </>
   );
 }
