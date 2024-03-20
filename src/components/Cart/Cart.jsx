933b665b94865e04139f01d8e04b6e2e7a5ca6af
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-03-20 23:39:27 +0530

    add Cart styles

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,18 +1,19 @@
 import { useOutletContext } from 'react-router-dom';
 import Item from './Item';
+import { StyledCart } from './Cart.styles';
 
 const Cart = () => {
   const cart = useOutletContext();
   let total = 0;
 
   return (
-    <div>
+    <StyledCart>
       {cart.map((item) => {
         total += item.total;
         return <Item key={item.id} item={item} />;
       })}
       <div className='total'>Checkout: {total} $</div>
-    </div>
+    </StyledCart>
   );
 };
 
---
+++ src/components/Cart/Cart.styles.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledCart = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px;
+
+  .total {
+    display: flex;
+    justify-content: flex-end;
+    padding: 10px;
+  }
+`;
